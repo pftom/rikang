6ddc1b671c6236ab4375c29b9135cb07d4183eb5
指尖泛出的繁华 <1043269994@qq.com>
2017-07-07 16:07:47 +0800

    post saga completed

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,18 @@
 import { all } from 'redux-saga/effects';
 
+//import userauth sagas
 import { loginFlow, registerFlow, clearFlow } from './userSaga';
+
+//import posts sagas
+import { watchGetPost, watchGetPosts } from './postSaga';
 
 export default function* rootSaga() {
   yield all([
     loginFlow(),
     registerFlow(),
     clearFlow(),
+
+    watchGetPost(),
+    watchGetPosts(),
   ]);
 }
---
+++ src/sagas/postSaga.js
@@ -0,0 +1,66 @@
+import { delay } from 'redux-saga';
+import { put, take, select, call, fork, cancel, cancelled } from 'redux-saga/effects';
+
+//import POSTS action constans
+import { 
+  GET_POSTS,
+  GET_POSTS_ERROR,
+  GET_POSTS_SUCCESS,
+  GET_SINGLE_POST,
+  GET_SINGLE_POST_ERROR,
+  GET_SINGLE_POST_SUCCESS,
+} from '../constants/';
+
+//import request api
+import request from '../configs/request';
+//import uri config api
+import { base, homeApi, homeSingleApi } from '../configs/config';
+//  import clear token api
+// import { clearItem, setItem } from '../actions/user';
+
+function* getSinglePost(payload) {
+  try {
+    const { id, token } = payload;
+    const { post } = yield call(request.get, base + homeSingleApi(id), null, token);
+    yield put({ type: GET_SINGLE_POST_SUCCESS, post });
+  } catch (error) {
+    yield put({ type: GET_SINGLE_POST_ERROR });
+  }
+}
+
+
+//LOGIN async actions handle function
+function* getPosts(payload) {
+  try {
+    const { posts } = yield call(request.get, base + homeApi.posts, null, payload);
+    yield put({ type: GET_POSTS_SUCCESS, payload: posts });
+  } catch(error) {
+    yield put({ type: GET_POSTS_ERROR });
+  }
+}
+
+//LOGIN async actions watch function
+function* watchGetPost() {
+  while (true) {
+    const { payload } = yield take(GET_SINGLE_POST);
+    // fork return a Task object for cancel later
+    yield call(getSinglePost, payload);
+  }
+}
+
+//LOGIN async actions watch function
+function* watchGetPosts() {
+  while (true) {
+    const { payload } = yield take(GET_POSTS);
+    // fork return a Task object for cancel later
+    yield call(getPosts, payload);
+  }
+}
+
+
+
+
+export {
+  watchGetPost,
+  watchGetPosts,
+}
