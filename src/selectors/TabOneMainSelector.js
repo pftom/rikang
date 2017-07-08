a3a270ce05751587f57c04fb897b1b6bdcdf5c28
指尖泛出的繁华 <1043269994@qq.com>
2017-07-08 20:34:29 +0800

    hospital selector completed

--- src/selectors/TabOneMainSelector.js
+++ src/selectors/TabOneMainSelector.js
@@ -7,6 +7,7 @@ const getDoctors = (state) => state.getIn(['home', 'doctors']);
 const getPosts = (state) => state.getIn(['home', 'posts']);
 const getLoadingError = (state) => state.getIn(['home', 'loadingError']);
 
+//home selector
 export const getHomeSelector = createSelector(
   [ getToken, getDoctors, getPosts, getLoadingError ],
   (token, doctors, posts, loadingError) => ({
@@ -18,6 +19,7 @@ export const getHomeSelector = createSelector(
 );
 
 
+//doctor selector
 const getDoctor = (state) => state.getIn(['home', 'doctor']);
 
 export const getDoctorSelector = createSelector(
@@ -27,11 +29,43 @@ export const getDoctorSelector = createSelector(
   }),
 );
 
+//post selector
 const getPost = (state) => state.getIn(['home', 'post']);
 
 export const getPostSelector = createSelector(
   [ getPost ],
   (post) => ({
     post,
+  }),
+);
+
+
+//all hospital selector
+const getHospitals = (state) => state.getIn(['hospital', 'hospitals']);
+
+export const getHospitalsSelector = createSelector(
+  [ getHospitals ],
+  (hospitals) => ({
+    hospitals,
+  }),
+);
+
+//hospital selector
+const getHospital = (state) => state.getIn(['hospital', 'hospital']);
+
+export const getHospitalSelector = createSelector(
+  [ getHospital ],
+  (hospital) => ({
+    hospital,
+  }),
+);
+
+//hospital all doctors selector
+const getHospitalDoctors = (state) => state.getIn(['hospital', 'hospitalDoctors']);
+
+export const getHospitalDoctorsSelector = createSelector(
+  [ getHospitalDoctors ],
+  (hospitalDoctors) => ({
+    hospitalDoctors,
   }),
 );
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,11 +1,25 @@
 import { getInputInitial } from './inputSelector';
-import { getHomeSelector, getDoctorSelector, getPostSelector } from './TabOneMainSelector';
+import { 
+  getHomeSelector, 
+
+  getDoctorSelector, 
+  getPostSelector,
+
+  getHospitalsSelector,
+  getHospitalSelector,
+  getHospitalDoctorsSelector,
+} from './TabOneMainSelector';
 
 
 export {
   getInputInitial,
 
   getHomeSelector,
+  
   getDoctorSelector,
   getPostSelector,
+
+  getHospitalsSelector,
+  getHospitalSelector,
+  getHospitalDoctorsSelector,
 }
