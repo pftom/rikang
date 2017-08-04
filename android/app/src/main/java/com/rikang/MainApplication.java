316aac935190d7e207463d2dbd37280b33111177
Tom <tom@TomdeMacBook-Pro.local>
2017-08-04 19:54:32 +0800

    make some modify for pingpp

--- android/app/src/main/java/com/rikang/MainApplication.java
+++ android/app/src/main/java/com/rikang/MainApplication.java
@@ -3,6 +3,7 @@ package com.rikang;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.pingplusplus.react.PingppPackage;
 import com.beefe.picker.PickerViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PingppPackage(),
             new PickerViewPackage(),
             new LinearGradientPackage(),
             new ImagePickerPackage()
--- src/components/TabTwo/styles/QaAnswerListStyle.js
+++ src/components/TabTwo/styles/QaAnswerListStyle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Dimensions, } from 'react-native';
+import { StyleSheet, Dimensions, Platform } from 'react-native';
 
 
 //import screen adapt util
